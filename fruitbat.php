3b5ea5d7bdb4c65f1d890c57d2da8bfc1b463f67
Ronald Waite <fyhrr@dymondsoft.com>
2015-08-06 15:10:21 +0900

    added functions to functions.php and other changes
    made fruitbat.html the index in .htaccess
    removed commented function from fruitbat.php
    added functions for types: text, header, submit,
    default.

--- fruitbat.php
+++ fruitbat.php
@@ -14,26 +14,6 @@ for ($i=0; $i < count($json_form); $i++) {
 	
 	// Handles the construction of the web page according to the objects given in the array
 	
-	/*switch ($type) {
-		case 'heading':
-			echo '<h2>' . $object["text"] . '</h2><br>';
-			break;
-		case 'text':
-			echo $object["label"] . ':<br><input type="text" name="' . 
-					$object["name"] . '" validator="' . $object["validator"] . '">';
-			break;
-			
-		case 'submit':
-			echo '<input type="submit" value="' . $object["text"] . '">';
-			break;
-		case 'textarea':
-			# code...
-			break;
-		default:
-			echo '<p style="color: #f00;">This element (' . $type . ') is not supported</p>';
-			break;
-	}*/
-	
 	$handlers[in_array($type, $handlers) ? $type : "default"]($object);
 }?>
 </form>
--- functions.php
+++ functions.php
@@ -1,20 +1,32 @@
 <?php
 /*public static function CreateElement($key, $value)
 {
-	
+
 }*/
 
 $handlers = array(
-	
-	// Input types
-	"text"=>function ($object, $postValue="")
-	{
-		# code...
-	}
-	
-	// Other types
-	);
 
-echo '<input type=' 
+	// Input types
+	"text"=>function ($object) 
+	{
+		echo $object["label"] . ':<br><input type="text" name="' .			$object["name"] . '" validator="' . $object["validator"] . '">';
+	},
+	"submit"=>function ($object)
+	{
+		echo '<input type="submit" value="' . $object["text"] . '">';
+	},
+
+	// Other types
+	"heading"=>function ($object) 
+	{
+		echo '<h2>' . $object["text"] . '</h2><br>';
+	},
+	"default"=>function ($object)
+	{
+		echo '<p style="color: #f00;">This element (' . $type . ') is not supported</p>';
+	}
+);
+
+//echo '<input type=' 
 ?>
 
